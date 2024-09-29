55239210f252128471fb11b147a036bac7d10914
icet-sharadha <icet.sharadha@gmail.com>
2024-09-29 15:31:59 +0530

    Service Implimantation

--- src/main/java/service/custom/CustomerService.java
+++ src/main/java/service/custom/CustomerService.java
@@ -1,4 +1,16 @@
 package service.custom;
 
-public interface CustomerService {
+import javafx.collections.ObservableList;
+import model.Customer;
+import service.SuperService;
+
+import java.util.List;
+
+public interface CustomerService extends SuperService {
+    boolean addCustomer(Customer customer);
+    ObservableList<Customer> getAllCustomers();
+    boolean updateCustomer(Customer customer);
+    boolean deleteCustomer(String id);
+    Customer searchCustomer(String id);
+    List<String> getAllCustomerIds();
 }
--- src/main/java/service/custom/ItemService.java
+++ src/main/java/service/custom/ItemService.java
@@ -1,4 +1,17 @@
 package service.custom;
 
-public interface ItemService {
+import javafx.collections.ObservableList;
+import model.Item;
+import model.OrderDetail;
+import service.SuperService;
+
+import java.util.List;
+
+public interface ItemService extends SuperService {
+    boolean addItem(Item item);
+    ObservableList<Item> getAllItems();
+    boolean updateItem(Item item);
+    boolean deleteItem(String id);
+    Item searchItem(String id);
+    boolean updateStock(List<OrderDetail> orderDetails);
 }
